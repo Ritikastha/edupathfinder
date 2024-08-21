930a83495d6cf8c43d611ef53043bc8aa3ba2129
Ritikastha <ritikashres2244@gmail.com>
2024-08-22 02:07:11 +0545

    Final Done

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -10,21 +10,21 @@ import { getSingleBasicinfoApi } from '../api/Api'
 import { getUserApi } from '../api/Api'
 import { updateBasicinfoApi } from '../api/Api'
 import { toast } from 'react-toastify';
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from 'axios';
 
 const EditProfile = () => {
-    
+
     const [phone, setPhone] = useState('')
     const [age, setAge] = useState('')
     const [level, setLevel] = useState()
     const [gender, setGender] = useState('')
-    const [address,setAddress] = useState('')
-    const [currentschool,setCurrentSchool] = useState('')
+    const [address, setAddress] = useState('')
+    const [currentschool, setCurrentSchool] = useState('')
     const [basicinfo, setBasicInfo] = useState([
 
     ]);
-    const [name, setName] = useState([]); 
+    const [name, setName] = useState([]);
     // const {id} = useParams()
 
     const id = localStorage.getItem('userId');
@@ -40,26 +40,26 @@ const EditProfile = () => {
     // const changePhone = (e) => {
     //     setPhone(e.target.value)
     //   }
-      
+
     //   const changeAge = (e) => {
     //       setAge(e.target.value)
     //   }
-  
+
     //   const changeLevel = (e) => {
     //       setLevel(e.target.value)
     //   }
-                                                                                             
+
     //   const changeGender = (e) => {
     //       setGender(e.target.value)
     //   }
-  
+
     //   const changeAddress= (e) => {
     //     setAddress(e.target.value)
     //   }
     //   const changeCurrentSchool= (e) => {
     //     setCurrentSchool(e.target.value)
     //   }
-     
+
     //   useEffect(() => {
     //     const fetchBasicInfo = async () => {
     //         try {
@@ -101,28 +101,17 @@ const EditProfile = () => {
                 toast.error('Error fetching basic info. Please try again later.');
             }
         };
-    
+
         fetchBasicInfo();
     }, [id]);
-    
-    
 
 
 
-      const handleSubmit =(e)=>{
+
+
+    const handleSubmit = (e) => {
         e.preventDefault()
         //console.log(name,phone,age,level,gender,address,currentschool)
-    
-        // // make a form data
-        // const formData=new FormData();
-        // formData.append('fullName',name)
-        // formData.append('phone',phone)
-        // formData.append('age',age)
-        // formData.append('level',level)
-        // formData.append('gender',gender)
-        // formData.append('address',address)
-        // formData.append('currentschool',currentschool)
-        // formData.append('userId',id)
 
         const data = {
             fullName: name,
@@ -135,20 +124,20 @@ const EditProfile = () => {
             user: id
         };
 
-        
-    
+
+
         // making api call
-        updateBasicinfoApi(id,data).then((res)=>{
-          if(res.data.success=true){
-            toast.success(res.data.message)
-            NavigationPreloadManager('/editprofile')
-          }else{
-            toast.error(res.data.message)
-          }
-        }).catch(err =>{
-          toast.error("Server error")
+        updateBasicinfoApi(id, data).then((res) => {
+            if (res.data.success = true) {
+                toast.success(res.data.message)
+                NavigationPreloadManager('/editprofile')
+            } else {
+                toast.error(res.data.message)
+            }
+        }).catch(err => {
+            toast.error("Server error")
         })
-      };
+    };
 
 
     return (
@@ -168,24 +157,24 @@ const EditProfile = () => {
                                     <h2>Edit Profile</h2>
                                 </div>
                                 {/* {basicinfo.map((info) => ( */}
-                                <form  className='form'>
-                                
-                                    
+                                <form className='form'>
+
+
                                     <div className="form-row">
-                                    {/* { name.map((item) => ( */}
+                                        {/* { name.map((item) => ( */}
                                         <div className="form-group col-md-6">
                                             <label htmlFor="name">Name</label>
                                             <div className="input-icon-wrapper">
-                                            <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="name" placeholder="Your Name"/>
-                                                <i  className="fas fa-edit input-icon"></i>
+                                                <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="form-control" id="name" placeholder="Your Name" />
+                                                <i className="fas fa-edit input-icon"></i>
                                             </div>
                                         </div>
-                                    {/* ))} */}
-                                        
+                                        {/* ))} */}
+
                                         <div className="form-group col-md-6">
                                             <label htmlFor="age">Age</label>
                                             <div className="input-icon-wrapper">
-                                                <input  value={age} onChange={(e) => setAge(e.target.value)}type="number" className="form-control " id="age" placeholder="Enter your age" />
+                                                <input value={age} onChange={(e) => setAge(e.target.value)} type="number" className="form-control " id="age" placeholder="Enter your age" />
                                                 <i className="fas fa-edit input-icon"></i>
                                             </div>
                                         </div>
@@ -199,33 +188,33 @@ const EditProfile = () => {
                                             </div>
                                         </div>
                                         <div className="form-group col-md-6">
-                                        <label>Gender</label><br />
-                                                <div className="form-check form-check-inline custom-form-check">
-                                                    <input  onChange={(e) => setGender(e.target.value)} className="form-check-input" type="radio" name="gender" id="male" value="male" checked={gender === "male"} />
-                                                    <label className="form-check-label" htmlFor="male">Male</label>
-                                                </div>
-                                                <div className="form-check form-check-inline custom-form-check">
-                                                    <input  onChange={(e) => setGender(e.target.value)}className="form-check-input" type="radio" name="gender" id="female" value="female" checked={gender === "female"} />
-                                                    <label className="form-check-label" htmlFor="female">Female</label>
-                                                </div>
-                                                <div className="form-check form-check-inline custom-form-check">
-                                                    <input  onChange={(e) => setGender(e.target.value)}className="form-check-input" type="radio" name="gender" id="other" value="other" checked={gender  === "other"} />
-                                                    <label className="form-check-label" htmlFor="other">Other</label>
-                                                </div>
-                                                </div>
+                                            <label>Gender</label><br />
+                                            <div className="form-check form-check-inline custom-form-check">
+                                                <input onChange={(e) => setGender(e.target.value)} className="form-check-input" type="radio" name="gender" id="male" value="male" checked={gender === "male"} />
+                                                <label className="form-check-label" htmlFor="male">Male</label>
+                                            </div>
+                                            <div className="form-check form-check-inline custom-form-check">
+                                                <input onChange={(e) => setGender(e.target.value)} className="form-check-input" type="radio" name="gender" id="female" value="female" checked={gender === "female"} />
+                                                <label className="form-check-label" htmlFor="female">Female</label>
+                                            </div>
+                                            <div className="form-check form-check-inline custom-form-check">
+                                                <input onChange={(e) => setGender(e.target.value)} className="form-check-input" type="radio" name="gender" id="other" value="other" checked={gender === "other"} />
+                                                <label className="form-check-label" htmlFor="other">Other</label>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="form-row">
                                         <div className="form-group col-md-6">
                                             <label htmlFor="address">Address</label>
                                             <div className="input-icon-wrapper">
-                                                <input value={address} onChange={(e) => setAddress(e.target.value)}type="text" className="form-control" id="address" placeholder="Enter your address" />
+                                                <input value={address} onChange={(e) => setAddress(e.target.value)} type="text" className="form-control" id="address" placeholder="Enter your address" />
                                                 <i className="fas fa-edit input-icon"></i>
                                             </div>
                                         </div>
                                         <div className="form-group col-md-6">
                                             <label htmlFor="currentSchool">Current School</label>
                                             <div className="input-icon-wrapper">
-                                                <input value={currentschool}  onChange={(e) => setCurrentSchool(e.target.value)}type="text" className="form-control" id="currentSchool" placeholder="Enter your current school" />
+                                                <input value={currentschool} onChange={(e) => setCurrentSchool(e.target.value)} type="text" className="form-control" id="currentSchool" placeholder="Enter your current school" />
                                                 <i className="fas fa-edit input-icon"></i>
                                             </div>
                                         </div>
@@ -234,7 +223,7 @@ const EditProfile = () => {
                                         <div className="form-group col-md-6">
                                             <label htmlFor="level">Level</label>
                                             <div className="input-icon-wrapper">
-                                                <select value={level}onChange={(e) => setLevel(e.target.value)}className="form-control" id="level">
+                                                <select value={level} onChange={(e) => setLevel(e.target.value)} className="form-control" id="level">
                                                     <option value="1">1</option>
                                                     <option value="2">2</option>
                                                     <option value="3">3</option>
@@ -248,10 +237,10 @@ const EditProfile = () => {
                                             </div>
                                         </div>
                                     </div>
-                                        
-                                    <button onClick={handleSubmit}type="submit" className="btn-save">Save</button>
+
+                                    <button onClick={handleSubmit} type="submit" className="btn-save">Save</button>
                                 </form>
-                                 
+
                             </div>
                         </div>
                     </div>
